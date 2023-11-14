63eac0961e6df3766fbb2e544998e5f0bdc9b5b8
kojikokojiko <kojiko430430@gmail.com>
2023-11-14 22:04:40 +0900

    sqlの変更

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -26,7 +26,19 @@ CREATE TABLE `isu_condition` (
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
+ALTER TABLE isu_condition ADD condition_level VARCHAR(15) NOT NULL DEFAULT 'info';
 
+UPDATE isu_condition SET condition_level = 'warning' WHERE (
+    TRUNCATE((LENGTH(`condition`) - LENGTH(REPLACE(`condition`, "=true", ''))) / LENGTH("=true"), 0) = 1
+);
+
+UPDATE isu_condition SET condition_level = 'warning' WHERE (
+    TRUNCATE((LENGTH(`condition`) - LENGTH(REPLACE(`condition`, "=true", ''))) / LENGTH("=true"), 0) = 2
+);
+
+UPDATE isu_condition SET condition_level = 'critical' WHERE (
+    TRUNCATE((LENGTH(`condition`) - LENGTH(REPLACE(`condition`, "=true", ''))) / LENGTH("=true"), 0) = 3
+);
 
 CREATE INDEX idx_jia_isu_uuid_timestamp ON isu_condition (jia_isu_uuid, timestamp DESC);
 
